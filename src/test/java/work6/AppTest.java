4bee16da54716c96ba300dc29bc2d1924c16190d
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-16 18:36:37 +0300

    Создание проекта, первый комит

---
+++ src/test/java/work6/AppTest.java
@@ -0,0 +1,20 @@
+package work6;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

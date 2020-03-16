b8a38d7c27655c79ad88baaf285e4ea3e5438210
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-16 19:07:19 +0300

    Второй комит, настройка

--- src/main/java/work6/task1/Person.java
+++ src/main/java/work6/task1/Person.java
@@ -5,6 +5,11 @@ public class Person {
     int age;
     String name;
 
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
     public int getAge() {
         return age;
     }
--- src/test/java/work6/task1/compTest.java
+++ src/test/java/work6/task1/compTest.java
@@ -6,11 +6,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class compTest {
+
     @Test
     public void compTest(){
         List personList = Arrays.asList(
                 new Person(22, "Ivan"),
-                new Person(12,"Ira"),
+                new Person(12,"Iran"),
                 new Person(53,"Vova"),
                 new Person(22,"Ivan")
         );

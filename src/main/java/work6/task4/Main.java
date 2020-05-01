707fedecfa67346cf9ad01af84c9fc9229458786
llARXANGELll <aramh@inbox.ru>
2020-05-01 05:48:48 +0300

    Исправления

--- src/main/java/work6/task4/Main.java
+++ src/main/java/work6/task4/Main.java
@@ -4,15 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        RemoveEvenLenght removeEvenLenght = new RemoveEvenLenght();
-        Set<String> set = new HashSet();
+        Set<String> set = new HashSet<String>();
         set.add("dsd");
         set.add("dsd1");
         set.add("dsd11");
         set.add("dsd2");
         set.add("dsd1");
+        RemoveEvenLenght removeEvenLenght = new RemoveEvenLenght();
+        System.out.println(removeEvenLenght.removeEvenLength(set));
     }
 }

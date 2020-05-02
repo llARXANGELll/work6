99029569e0d56a14e0e799dc20d8edb1ffa7f673
llARXANGELll <aramh@inbox.ru>
2020-05-02 21:28:33 +0300

    Все поправил

--- src/main/java/work6/task1/PersonSuperComparator.java
+++ src/main/java/work6/task1/PersonSuperComparator.java
@@ -3,19 +3,17 @@ package work6.task1;
 
 import java.util.Comparator;
 
-public class PersonSuperComparator implements Comparator {
+public class PersonSuperComparator implements Comparator<Person> {
 
     @Override
-    public int compare(Object o1, Object o2) {
-        Person p1 = (Person) o1;
-        Person p2 = (Person) o2;
+    public int compare(Person o1, Person o2) {
 
-        if (p1.getAge() > p2.getAge()) {
+        if ((o1).getAge() > (o2).getAge()) {
             return 1;
-        } else if (p1.getAge() < p2.getAge()) {
+        } else if ((o1).getAge() < (o2).getAge()) {
             return -1;
         }
 
-        return p1.getName().compareTo(p2.getName());
+        return (o1).getName().compareTo((o2).getName());
     }
 }
--- src/main/java/work6/task4/Main.java
+++ src/main/java/work6/task4/Main.java
@@ -5,12 +5,12 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> set = new HashSet<String>();
+        Set<String> set = new HashSet<>();
         set.add("dsd");
         set.add("dsd1");
         set.add("dsd11");
         set.add("dsd2");
-        set.add("dsd1");
+        set.add("dsd111");
         RemoveEvenLenght removeEvenLenght = new RemoveEvenLenght();
     }
 }

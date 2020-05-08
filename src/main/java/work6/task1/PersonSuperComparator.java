09cf304eec324861abdfb937be65dca831dd1b45
llARXANGELll <aramh@inbox.ru>
2020-05-08 17:33:02 +0300

    Поправил

--- src/main/java/work6/task1/PersonSuperComparator.java
+++ src/main/java/work6/task1/PersonSuperComparator.java
@@ -7,13 +7,11 @@ public class PersonSuperComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-
         if ((o1).getAge() > (o2).getAge()) {
             return 1;
         } else if ((o1).getAge() < (o2).getAge()) {
             return -1;
         }
-
         return (o1).getName().compareTo((o2).getName());
     }
 }
